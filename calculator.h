73d02d4f32ec2d5a44bc9ce1d8f4cb0b4b632f64
Andrew Ng <ngandrew1337@gmail.com>
2021-02-04 21:35:57 -0900

    Create initial files

---
+++ calculator.h
@@ -0,0 +1,10 @@
+#ifndef FILE_CALCULATOR_H_INCLUDED
+#define FILE_CALCULATOR_H_INCLUDED
+
+#include <string>
+
+
+int stringCalculate(const std::string &str);
+
+
+#endif  // FILE_CALCULATOR_H_INCLUDED
