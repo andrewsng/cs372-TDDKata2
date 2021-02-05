b30897d615798274f5d3249be7b6ae641c4b00f9
Andrew Ng <ngandrew1337@gmail.com>
2021-02-04 22:00:32 -0900

    Write tests for Step 3

--- tests.cpp
+++ tests.cpp
@@ -12,3 +12,9 @@ TEST_CASE( "A single number returns the value" ) {
     REQUIRE( stringCalculate("24") == 24);
     REQUIRE( stringCalculate("578") == 578);
 }
+
+TEST_CASE( "Two numbers, comma delimited, returns the sum" ) {
+    REQUIRE( stringCalculate("13, 48") == 51);
+    REQUIRE( stringCalculate("321, 8") == 329);
+    REQUIRE( stringCalculate("4,82") == 86);
+}
