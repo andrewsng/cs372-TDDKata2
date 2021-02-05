77920399351a46289a12fa559c8b1f0a13ae3415
Andrew Ng <ngandrew1337@gmail.com>
2021-02-04 21:48:21 -0900

    Write tests for Step 2

--- tests.cpp
+++ tests.cpp
@@ -6,3 +6,9 @@
 TEST_CASE( "An empty string returns zero" ) {
     REQUIRE( stringCalculate("") == 0 );
 }
+
+TEST_CASE( "A single number returns the value" ) {
+    REQUIRE( stringCalculate("6") == 6);
+    REQUIRE( stringCalculate("24") == 24);
+    REQUIRE( stringCalculate("578") == 578);
+}
